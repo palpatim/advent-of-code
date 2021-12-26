a34a2c9adf984ba0c404dcee5acad3e3a1a9147a
Tim Schmelter <schmelte@amazon.com>
2021-12-25 20:42:53 -0800

    2021, day24 WIP

---
+++ 2021/day24/Sources/day24/day24.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+public struct day24 {
+    static let initialValue = [
+        "x": 0,
+        "y": 0,
+        "z": 0,
+        "w": 0
+    ]
+
+    public func parseInput(_ input: String) -> Program {
+        let lines = input.components(separatedBy: "\n")
+        return program
+    }
+
+}
